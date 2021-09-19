1096b14acd43ece0eeed1bcb699caa9d42b623c3
fumi <54024213+F-UMI@users.noreply.github.com>
2021-09-19 19:29:35 +0900

    21.09.19 (2)
    ch06_19 문제 해결

---
+++ this_is_java/src/ch06_19/Account.java
@@ -0,0 +1,22 @@
+package ch06_19;
+
+public class Account {
+	final static int MIN_BALANCE = 0;
+	final static int MAX_BALANCE = 1000000;
+	private int balance;
+	
+	public void setBalance(int balance) {
+		if ( MIN_BALANCE > balance || balance > MAX_BALANCE) {
+			getBalance();
+		}
+		else {
+			this.balance = balance;			
+		}
+	}
+	
+	public int getBalance() {
+
+		return balance;
+	}
+	
+}
