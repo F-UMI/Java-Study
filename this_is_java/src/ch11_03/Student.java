675486160e1bfdd12944d5155ee8fe6d5838d894
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-09-26 20:00:45 +0900

    +이것이 자바다 문제풀이
    2021.09.26

---
+++ this_is_java/src/ch11_03/Student.java
@@ -0,0 +1,13 @@
+package ch11_03;
+
+public class Student {
+	private String studentNum;
+	
+	public	Student(String studentNum) {
+		this.studentNum = studentNum;
+	}
+	public String getStudentNum() {
+		return studentNum;
+	}
+	// 시작
+}
