440cc1c4047be84c59084c49af536d03b7487772
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-09-30 20:08:53 +0900

    +modify Solution.java
    21.9.30

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,39 +1,108 @@
-
-//Lv1_ 제일 작은수 제거하기 
-//제일 작은수가 여러개 있을 경우 ex 1이 3개 일때 3개를 모두 없애야 하기 때문?
-//내일 다시 풀기
-import java.util.Arrays;
-
+//  다시 생각 해봐야할듯
 class Solution {
 	public int[] solution(int[] arr) {
 		int[] answer = {};
-		if (arr.length < 2) {
-			answer = new int[1];
-			answer[0] = -1;
-			return answer;
+		int minNum = 0;
+		for (int i = 0; i < arr.length-1; i++) {
+			if (arr[i] < arr[i+1]) {
+				minNum = arr[i];
+			}
 		}
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] < arr[j]) {
-					int tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-				}
+			if (arr[i]) {
+				
 			}
-
 		}
-		answer = new int[arr.length - 1];
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = arr[i];
-			System.out.println(answer[i]);
+		
+		
+		while () {
+			
 		}
-
 		return answer;
 	}
-
-	public static void main(String[] args) {
-		int[] arr = { 3, 2, 1,4,5,6,7,2,3,5 , 5, 1 };
-		Solution sol = new Solution();
-		sol.solution(arr);
-	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import java.util.Arrays;
+//
+//class Solution {
+//	public int[] solution(int[] arr) {
+//		int[] answer = {};
+//		if (arr.length < 2) {
+//			answer = new int[1];
+//			answer[0] = -1;
+//			return answer;
+//		}
+//		
+//		for (int i = 1; i < answer.length; i++) {
+//			if (arr[i-1] == arr[i]) {
+//				answer = new int[1];
+//				answer[0] = -1;
+//				return answer;
+//			}
+//		} 
+//		
+//		for (int i = 0; i < arr.length; i++) {
+//			for (int j = i + 1; j < arr.length; j++) {
+//				if (arr[i] < arr[j]) {
+//					int tmp = arr[i];
+//					arr[i] = arr[j];
+//					arr[j] = tmp;
+//				}
+//			}
+//		}
+//		int startIndex = findMinNumIndex(arr);
+//		answer = new int[startIndex];
+//		for (int i = 0; i < answer.length; i++) {
+//			answer[i] = arr[i];
+//			System.out.println(answer[i]);
+//		}
+//
+//		return answer;
+//	}
+//
+//	public int findMinNumIndex (int[] arr){
+//		
+//		
+//		String minNum = Integer.toString(arr[arr.length-1]);
+//		String[] numStrArr = new String[arr.length];
+//		for (int i = 0; i < arr.length; i++) {
+//			numStrArr[i] =Integer.toString(arr[i]);
+//		}
+//		String numStr = String.join("", numStrArr);
+//		int startIndex = numStr.indexOf(minNum);
+//	
+//		return startIndex;
+//	}
+//
+//	public static void main(String[] args) {
+//		int[] arr = { 3, 2, 1, 4, 5, 6, 7, 2, 3, 5, 5, 1 };
+//		Solution sol = new Solution();
+//		sol.solution(arr);
+//	}
+//}
