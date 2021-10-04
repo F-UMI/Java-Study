ef39e146baac4aa0f1186dd7634857efc3e0caa5
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-10-04 19:31:43 +0900

    +Lv1_자연수 뒤집어 배열로 만들기
    21.10.04

--- Solution/src/numBackToArray.java
+++ Solution/src/numBackToArray.java
@@ -1,38 +1,44 @@
-//Lv1_자연수 뒤집어 배열로 만들기
+//Lv1_자연수 뒤집어 배열로 만들기 (5) / 21.10.04
 class numBackToArray {
-    public long[] solution(long n) {
+	public long[] solution(long n) {
 
-        long[] answer = {};
+		long[] answer = {};
 		String num = Long.toString(n);
 		String[] numDivide = num.split("");
 		long[] str = new long[numDivide.length];
 		for (int i = 0; i < numDivide.length; i++) {
 			str[i] = Long.parseLong(numDivide[i]);
 		}
-		
-		for (int i = 0; i < str.length ; i++) {
-			for (int j = i + 1; j < str.length; j++) {
-				changeIndex(str, i, j);
+		int j = str.length - 1;
+
+		if (str.length % 2 == 0) {
+			for (int i = 0; i < str.length / 2; i++) {
+				long tmp = str[i];
+				str[i] = str[j];
+				str[j] = tmp;
+				j--;
+				System.out.println(j);
 			}
-            System.out.println(str[i]);
+		} else if (str.length % 2 == 1) {
+			for (int i = 0; i < (str.length - 1) / 2; i++) {
+				long tmp = str[i];
+				str[i] = str[j];
+				str[j] = tmp;
+				j--;
+			}
 		}
-        answer = new long[str.length];
-        for(int i = 0; i <str.length; i++) {
-            answer[i] = str[i];
-        }
-        return answer;
-            
-        
-}
-    public void changeIndex(long[] array, int indexA, int indexB) {
-		if (array[indexA] < array[indexB]) {
-			long tmp = array[indexA];
-			array[indexA] = array[indexB];
-			array[indexB] = tmp;
+		answer = new long[str.length];
+		for (int i = 0; i < str.length; i++) {
+			answer[i] = str[i];
+			System.out.println(answer[i]);
 		}
+		return answer;
 
 	}
-    public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+		numBackToArray sol = new numBackToArray();
+		sol.solution(29484);
+
 	}
 }
