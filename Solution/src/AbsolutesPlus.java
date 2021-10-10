be48a77cb45ea81d05ebdff5385769c424654b4f
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-10-10 16:56:08 +0900

    Create AbsolutesPlus.java
    21.10.10

---
+++ Solution/src/AbsolutesPlus.java
@@ -0,0 +1,19 @@
+//Lv1_음양 더하기(1) 21.10.10 //https://programmers.co.kr/learn/courses/30/lessons/76501
+class AbsolutesPlus {
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer = 0;
+		for (int i = 0; i < absolutes.length; i++) {
+			if (signs[i] == true) {
+				continue;
+			}
+			if (signs[i] == false) {
+				absolutes[i] = absolutes[i] * (-1);
+			}
+		}
+		for (int i = 0; i < absolutes.length; i++) {
+			answer += absolutes[i];
+		}
+
+		return answer;
+	}
+}
