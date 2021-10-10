0a6d2eacdaea782df13d507b509527808bf3e9f5
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-10-10 15:29:57 +0900

    Create Solution.java
    static 변수 사용 해결

---
+++ Solution/src/Solution.java
@@ -0,0 +1,14 @@
+//Lv_1 x만큼  간격있는 n개의 숫자 (4)  //  https://programmers.co.kr/learn/courses/30/lessons/12954
+class Solution {
+	static long constants;
+	public long[] solution(int x, int n) {
+		long[] answer = new long[n];
+		constants = x;
+			for (int i = 1; i <= n; i++) {
+				answer[i-1] = constants * i;
+			}
+
+
+			return answer;
+	}
+}
