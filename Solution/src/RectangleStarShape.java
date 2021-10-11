30e72ccf27f069c2280ec7654ae74d2046c479df
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-10-11 11:33:27 +0900

    Update RectangleStarShape.java
    easy

--- Solution/src/RectangleStarShape.java
+++ Solution/src/RectangleStarShape.java
@@ -1,17 +1,17 @@
+//Lv1_직사각형 별찍기(2) 21.10.11 // https://programmers.co.kr/learn/courses/30/lessons/12969
 import java.util.Scanner;
 
 public class RectangleStarShape {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.print("\"");
-        for (int i = 0; i < b; i++) {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		for (int i = 0; i < b; i++) {
 			for (int j = 0; j < a; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-        System.out.println("\"");
-    }
+	}
 }
