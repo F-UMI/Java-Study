8f702c6dc3f303129bc8587007930e8a79cead58
윤섭 <54024213+F-UMI@users.noreply.github.com>
2021-10-11 11:20:29 +0900

    Create WaterMelon.java
    easy

---
+++ Solution/src/WaterMelon.java
@@ -0,0 +1,16 @@
+//Lv1_수박수박수박수박수박수? 21.10.11 // https://programmers.co.kr/learn/courses/30/lessons/12922?language=java
+class WaterMelon {
+    public String solution(int n) {
+        String answer = "";
+        
+        for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				answer = answer + "수";
+			}
+			else {
+				answer = answer +"박";
+			}
+		}
+        return answer;
+    }
+}
