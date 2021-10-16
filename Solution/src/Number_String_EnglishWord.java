51d7f21b7be6d1be7023122b8cf4d660fcf003dc
supsuphae <zksdbstjq@gmail.com>
2021-10-16 19:24:38 +0900

    +update Number_String_EnglishWord.java

--- Solution/src/Number_String_EnglishWord.java
+++ Solution/src/Number_String_EnglishWord.java
@@ -1,25 +1,23 @@
-//Lv1_숫자 문자열과 영단어- 2021 카카오채용연계형 인턴쉽 // 21.10. 16 //https://programmers.co.kr/learn/courses/30/lessons/81301
-//40점?
-class Number_String_EnglishWord {
+//Lv1_숫자 문자열과 영단어- 2021 카카오채용연계형 인턴쉽(2) // 21.10. 16 //https://programmers.co.kr/learn/courses/30/lessons/81301
+class Solution {
     public int solution(String s) {
         int answer = 0;
         int[] array = new int[s.length()];
         int index = 0;
         String[] strings = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for (int i = 0; i < s.length(); i++) {
-            System.out.print("i = " + i + "|||");
+//            System.out.print("i = " + i + "|||");
             if (0 <= s.charAt(i) && s.charAt(i) <= 89) {
                 array[index] = Character.getNumericValue(s.charAt(i));
-                System.out.println(array[index]);
+//                System.out.println(array[index]);
                 index++;
                 continue;
             } else {
-                //index 상 뒤에있어도 stings[j]에서 해당수가 먼저 나오면 가운데 값 스킵. 2  0 --> 0
                 for (int j = 0; j < strings.length; j++) {
-                    if (s.indexOf(strings[j], i) != -1) {
+                    if (s.indexOf(strings[j], i) == i ) {
                         array[index] = j;
-                        System.out.println("j = " + j);
-                        System.out.println(array[index]);
+//                        System.out.println("j = " + j);
+//                        System.out.println(array[index]);
                         index++;
                         i = i + (strings[j].length() - 1);
                         break;
@@ -40,7 +38,7 @@ class Number_String_EnglishWord {
     }
 
     public static void main(String[] args) {
-        Number_String_EnglishWord solution = new Number_String_EnglishWord();
+        Solution solution = new Solution();
         solution.solution("1zerotwozero3");
 
     }
