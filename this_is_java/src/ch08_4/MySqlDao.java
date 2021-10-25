e15e955f1468f9d893915ce5f90c5cf68c7bd954
윤섭 <zksdbstjq@gmail.com>
2021-10-25 20:07:49 +0900

    Feat : 인터페이스 확인문제 4번 구현
    추상 메소드 구현후 구현 클래스에서 Override 완료
    
    (#p385)

---
+++ this_is_java/src/ch08_4/MySqlDao.java
@@ -0,0 +1,23 @@
+package ch08_4;
+
+public class MySqlDao implements DataAccessObject {
+    @Override
+    public void select() {
+
+    }
+
+    @Override
+    public void insert() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
---
+++ this_is_java/src/ch08_4/OracleDao.java
@@ -0,0 +1,23 @@
+package ch08_4;
+
+public class OracleDao implements DataAccessObject {
+    @Override
+    public void select() {
+
+    }
+
+    @Override
+    public void insert() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
