5c5c91ba190a04ebe4236cc5d91fd7e67d9a33f2
윤섭 <zksdbstjq@gmail.com>
2021-10-28 19:13:49 +0900

    Feat : 9강 확인문제 4번 Car클래스 내부 멤버클래스의 객체를 생성하기 p417

---
+++ this_is_java/src/ch09_04/Car.java
@@ -0,0 +1,6 @@
+package ch09_04;
+
+public class Car {
+    class Tire { }
+    static class Engine { }
+}
---
+++ this_is_java/src/ch09_04/NestedClassExample.java
@@ -0,0 +1,10 @@
+package ch09_04;
+
+public class NestedClassExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        Car.Tire tire = myCar.new Tire(); // 인스턴스 클래스는 해당 클래스를 감싸고 있는 클래스의 객체가 생성 되어야 선언이 가능하다
+        Car.Engine engine = new Car.Engine(); // 정적 클래스는 감싸고 있는 클래스의 객체 필요없이 바로 선언이 가능하다.
+    }
+}
