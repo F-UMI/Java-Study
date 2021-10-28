c8040983d4c737c5d4fc23f929e2624a6cd8fa47
윤섭 <zksdbstjq@gmail.com>
2021-10-28 19:50:20 +0900

    Feat : 9강 확인문제 6번 컴파일 에러 찾기 / 익명구현객체 로컬변수 사용시 final 특성을 가지지 않았다. p417

---
+++ this_is_java/src/ch09_06/Chatting.java
@@ -0,0 +1,30 @@
+package ch09_06;
+
+public class Chatting {
+    void startChat(String chatId) {
+        final String nickName = chatId;
+//        String nickName = null;
+//        nickName = chatId;
+        Chat chat = new Chat() {
+            @Override
+            void start() {
+                while (true) {
+                    String inputData = "안녕하세여";
+                    String message = "[" + nickName + "]" + inputData;
+                    sendMessage(message);
+                }
+            }
+        };
+
+    }
+
+    class Chat {
+        void start() {
+        }
+
+        void sendMessage(String message) {
+        }
+    }
+
+}
+
