49c000d007523005e5477b6556a4399e949c847e
윤섭 <zksdbstjq@gmail.com>
2021-10-31 16:16:53 +0900

    Feat : 11강 확인문제 4번 문제풀이 객체 해시코드 재정의 (#p567)

--- this_is_java/src/ch11_03/Student.java
+++ this_is_java/src/ch11_03/Student.java
@@ -1,13 +1,29 @@
 package ch11_03;
 
 public class Student {
-	private String studentNum;
-	
-	public	Student(String studentNum) {
-		this.studentNum = studentNum;
-	}
-	public String getStudentNum() {
-		return studentNum;
-	}
-	// 시작
+    private String studentNum;
+
+    public Student(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Student) {
+            Student compareStudent = (Student) obj;
+            if(compareStudent.studentNum == this.studentNum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.parseInt(this.studentNum) ;
+    }
 }
