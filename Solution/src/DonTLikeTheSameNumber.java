fc3f1bb30e28092ae27f2364b0718a0a6545f639
윤섭 <zksdbstjq@gmail.com>
2021-11-01 20:16:15 +0900

    Feat : 같은숫자는 싫어 예제 구현 해결 X

---
+++ Solution/src/DonTLikeTheSameNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class DonTLikeTheSameNumber {
+    public int[] solution(int[] arr) {
+
+        int[] answer = new int[arr.length];
+        answer[0] = arr[0];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++){
+            if(answer[count] != arr[i]) {
+                count++;
+            }
+        }
+        answer = new int[count];
+        answer[0] = arr[0];
+        count = 0;
+        for (int i = 0; i < arr.length; i++){
+            if(answer[count] != arr[i]) {
+                count++;
+                answer[count] = arr[i];
+            }
+        }
+        for (int i = 0; i <answer.length; i++){
+            System.out.println(answer[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DonTLikeTheSameNumber solution = new DonTLikeTheSameNumber();
+        int arr[] = {1, 1, 3, 3, 0, 1, 1};
+        solution.solution(arr);
+    }
+}
