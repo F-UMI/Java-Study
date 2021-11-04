dafc6606c114d49e4654802a4c020e341b63bc50
윤섭 <zksdbstjq@gmail.com>
2021-11-04 19:46:51 +0900

    Feat : 12장 sec 9 예제 구현 스레드풀 개념 / 작업 생성및 처리요청 #626

---
+++ this_is_java/src/chap12_MULT_THREAD/sec09/exam01_excute_submit/ExecuteVsSubmitExample.java
@@ -0,0 +1,28 @@
+package chap12_MULT_THREAD.sec09.exam01_excute_submit;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecuteVsSubmitExample {
+    public static void main(String[] args) throws  InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++){
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)executorService;
+                    int poolSize = threadPoolExecutor.getPoolSize();
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("작업 처리 스레드 이름" + threadName+ "   총 스레드 수"+ poolSize);
+                    int value = Integer.parseInt("삼");
+                }
+            };
+//            executorService.execute(runnable);
+            executorService.submit(runnable);
+            Thread.sleep(10);
+        }
+        executorService.shutdown();
+    }
+}
