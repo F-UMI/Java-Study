fdcc9286637fb2271d5fd60af5e6f21ec1da714c
yunsup <zksdbstjq@gmail.com>
2022-01-02 19:19:18 +0900

    Feat : 콜라츠 추측 문제 풀이 완료

--- Solution/src/CollatzSuggestion.java
+++ Solution/src/CollatzSuggestion.java
@@ -1,20 +1,23 @@
 //Lv_1 콜라츠 추측  num 의 범위로 테스트 3 오류?
 public class CollatzSuggestion {
-	public int solution(double num) {
-        int answer = 0;
-        if (num == 1) {
-            answer = 0;
-        }
-        
-		while (num!= 1) {
-		if (answer >= 500) {
-			answer = -1;
-		}
-		num = (num % 2 ==0) ? (num/2) : (num*3 +1);
-        answer++;
-		}
-		
-		return answer;
-	}
 
+    public int solution(double num) {
+        int answer = 0;
+
+        while (true) {
+            if (num == 1) {
+                return answer;
+            } else if (answer > 500) {
+                answer = -1;
+                break;
+            }
+            answer++;
+            num = isEven(num) ? num / 2 : num * 3 + 1;
+        }
+        return answer;
+    }
+
+    private boolean isEven(double number) {
+        return number % 2 == 0;
+    }
 }
