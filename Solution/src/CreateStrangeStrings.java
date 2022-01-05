2507c2d0341998ab930a4ef06f04a529413c2a43
yunsup <zksdbstjq@gmail.com>
2022-01-05 20:04:07 +0900

    Docs : 이상한문제열 만들기 문제 메소드 수정

--- Solution/src/CreateStrangeStrings.java
+++ Solution/src/CreateStrangeStrings.java
@@ -1,28 +1,35 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //Lv1_ 이상한 문자열 만들기 (?) // 공백이 여러개 일경우 해결하지 못한듯? 68.8 / 21.10.07
 class CreateStrangeStrings {
-    private static final String FIRST_REGEX = " ";
-	private static final String SECOND_REGEX = "";
 
-    public String solution(String s) {
-        Stream<String> stream = Arrays.stream(s.split(FIRST_REGEX));
-        stream.map(s1 -> s1.split(SECOND_REGEX)).peek()
+  private static final String FIRST_REGEX = " ";
+  private static final String SECOND_REGEX = "";
 
+  public String solution(String s) {
+    Stream<String> stream = Arrays.stream(s.split(FIRST_REGEX));
+  }
+
+  private static String toWeirdString(String[] strings) {
+      String weirdString = "";
+    for (int i = 0; i < strings.length; i++) {
+        isEven(i) ? toUpperString(strings[i]) : toLowerString(strings[i]);
     }
+  }
 
-	private static String toWeirdString(String s) {
+  private static boolean isEven(int i) {
+    return i % 2 == 0;
+  }
 
-	}
+  private static String toUpperString(String string) {
+    return string.toUpperCase();
+  }
 
-    private static String toUpperString(String string) {
-        return string.toUpperCase();
-    }
-
-    private static String toLowerString(String string) {
-        return string.toLowerCase();
-    }
+  private static String toLowerString(String string) {
+    return string.toLowerCase();
+  }
 
 
 /*		String answer = "";
