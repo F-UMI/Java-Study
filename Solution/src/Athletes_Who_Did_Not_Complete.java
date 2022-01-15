b69686eee2580923cb613e057b7f43b8f8ae418c
yunsup <zksdbstjq@gmail.com>
2022-01-15 16:51:14 +0900

    Feat : 완주하지 못한 선수 구현 완료

--- Solution/src/Athletes_Who_Did_Not_Complete.java
+++ Solution/src/Athletes_Who_Did_Not_Complete.java
@@ -1,19 +1,46 @@
 //효율성 실패
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 public class Athletes_Who_Did_Not_Complete {
-    public String solution(String[] participant, String[] completion) {
 
-        List<String> participantList = new ArrayList<>(Arrays.asList(participant));
+  public String solution(String[] participant, String[] completion) throws Exception {
+
+    Map<String, Integer> participantMap = new HashMap<>();
+    putParticipant(participant, participantMap);
+    putCompletion(completion, participantMap);
+    return verifyParticipant(participantMap);
+
+  }
+/*        List<String> participantList = new ArrayList<>(Arrays.asList(participant));
         List<String> completionList = new ArrayList<>(Arrays.asList(completion));
         Stream<String> participantStream = Arrays.stream(participant);
         participantStream.filter(completionList::contains).peek(participantList::remove).forEach(completionList::remove);
-        return participantList.get(0);
-    }
+        return participantList.get(0);*/
 
+  private static void putParticipant(String[] participant, Map<String, Integer> participantMap) {
+
+    for (String player : participant) {
+      participantMap.put(player, participantMap.getOrDefault(player, 0) + 1);
+    }
+  }
+
+  private static void putCompletion(String[] completion, Map<String, Integer> participantMap) {
+    for (String player : completion) {
+      participantMap.put(player, participantMap.get(player) - 1);
+    }
+  }
+
+  private static String verifyParticipant(Map<String, Integer> participantMap) throws Exception {
+    for (Entry<String, Integer> participantEntry : participantMap.entrySet()) {
+      if (participantEntry.getValue() != 0) {
+        return participantEntry.getKey();
+      }
+    }
+    throw new RuntimeException();
+  }
 
 }
