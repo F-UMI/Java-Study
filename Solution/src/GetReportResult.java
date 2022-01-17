67551c0243aec2fbbbf000244c6f6d11854afb19
yunsup <zksdbstjq@gmail.com>
2022-01-17 19:30:06 +0900

    Feat : 신고 결과 받기 구현 완료

--- Solution/src/GetReportResult.java
+++ Solution/src/GetReportResult.java
@@ -6,67 +6,69 @@ import java.util.stream.IntStream;
 
 public class GetReportResult {
 
-  private static final String REGEX = " ";
-  private static final int ZERO = 0;
-  private static final int EVEN = 2;
+    private static final String REGEX = " ";
+    private static final int ZERO = 0;
+    private static final int EVEN = 2;
 
 
-  public int[] solution(String[] id_list, String[] report, int k) {
-    int[] answer = new int[id_list.length];
-    List<String> duplicateIDList = getSplitList(toDeduplication(report));
-    duplicateIDList.forEach(System.out::println);
-    int[][] ReportResultArray = new int[id_list.length][id_list.length];
-    IntStream.range(ZERO, duplicateIDList.size())
-        .filter(this::isEven)
-        .forEach(index -> recordReport(index, duplicateIDList, id_list, ReportResultArray));
-    return discriminationReportCount(ReportResultArray, answer, k);
-    // 누적수가 k 이상일때 해당 열값이 1인 인덱스의 개수를 리턴하면 된다.
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        List<String> duplicateIDList = getSplitList(toDeduplication(report));
+/*        duplicateIDList.forEach(System.out::println);*/
+        int[][] ReportResultArray = new int[id_list.length][id_list.length];
+        IntStream.range(ZERO, duplicateIDList.size())
+                .filter(this::isEven)
+                .forEach(index -> recordReport(index, duplicateIDList, id_list, ReportResultArray));
+        return discriminationReportCount(ReportResultArray, answer, k);
+        // 누적수가 k 이상일때 해당 열값이 1인 인덱스의 개수를 리턴하면 된다.
 
-  }
-  private int[] discriminationReportCount(int[][] reportResultArray, int[] answer, int k) {
-    int[] reportedCount = new int[answer.length];
-    for (int i = 0; i < reportResultArray.length; i++) {
-      for (int j = 0; j < reportResultArray.length; j++) {
-        reportedCount[i] += reportResultArray[j][i];
-      }
     }
-    return answer;
-  }
 
-  private void recordReport(int index, List<String> duplicateIDList, String[] id_list,
-      int[][] ReportResultArray) {
-    List<String> id_toList = Arrays.stream(id_list).collect(Collectors.toList());
-    int row = id_toList.lastIndexOf(duplicateIDList.get(index));
-    int column = id_toList.lastIndexOf(duplicateIDList.get(index + 1));
-    ReportResultArray[row][column]++;
-    System.out.println(row +" "+ + column + " " + ReportResultArray[row][column]);
-  }
+    private int[] discriminationReportCount(int[][] reportResultArray, int[] answer, int k) {
+        int[] reportedCount = new int[answer.length];
+        for (int i = 0; i < reportResultArray.length; i++) {
+            for (int j = 0; j < reportResultArray.length; j++) {
+                reportedCount[i] += reportResultArray[j][i];
+            }
+        }
 
-  private boolean isEven(int index) {
-    return index % EVEN == ZERO;
-  }
+        for (int i = 0; i < reportedCount.length; i++) {
+            if (reportedCount[i] >= k) {
+                for (int j = 0; j < reportResultArray.length; j++) {
+                    if (reportResultArray[j][i] != 0) {
+                        answer[j]++;
+                    }
+                }
+            }
+        }
 
-  private static String[] toDeduplication(String[] report) {
-    return Arrays.stream(report)
-        .distinct()
-        .toArray(String[]::new);
-  }
-
-  private static List<String> getSplitList(String[] report) {
-    return Arrays.stream(report).map(id -> id.split(REGEX))
-        .flatMap(Arrays::stream)
-        .collect(Collectors.toList());
-  }
-
-
-  public static void main(String[] args) {
-    String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-    String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
-    int k = 2;
-    GetReportResult solution = new GetReportResult();
-    int[] array = solution.solution(id_list, report, k);
-    for (int i : array) {
-      System.out.println(i);
+        return answer;
     }
-  }
+
+    private void recordReport(int index, List<String> duplicateIDList, String[] id_list,
+                              int[][] ReportResultArray) {
+        List<String> id_toList = Arrays.stream(id_list).collect(Collectors.toList());
+        int row = id_toList.lastIndexOf(duplicateIDList.get(index));
+        int column = id_toList.lastIndexOf(duplicateIDList.get(index + 1));
+        ReportResultArray[row][column]++;
+/*        System.out.println(row + " " + +column + " " + ReportResultArray[row][column]);*/
+    }
+
+    private boolean isEven(int index) {
+        return index % EVEN == ZERO;
+    }
+
+    private static String[] toDeduplication(String[] report) {
+        return Arrays.stream(report)
+                .distinct()
+                .toArray(String[]::new);
+    }
+
+    private static List<String> getSplitList(String[] report) {
+        return Arrays.stream(report).map(id -> id.split(REGEX))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+    }
+
+
 }
