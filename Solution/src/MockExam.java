350acbca4485c45899a128e89ea1fc697873d6cf
yunsup <zksdbstjq@gmail.com>
2022-01-18 19:52:59 +0900

    Add : 모의고사 문제풀이 구현중

---
+++ Solution/src/MockExam.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MockExam {
+
+  final static int[] FIRST = {1,2,3,4,5};
+  final static int[] SECOND = {2,1,2,3,2,4,2,5};
+  final static int[] THIRD = {3,3,1,1,2,2,4,4,5,5};
+
+  public int[] solution(int[] answers) {
+    int[] answer = {};
+    return answer;
+  }
+
+
+  private static int first_verify(int[] answer) {
+    IntStream.rangeClosed(0, answer[answer.length])
+        .
+  }
+
+  private static int second_verify(int[] answer) {
+  }
+
+  private static int third_verify(int[] answer) {
+  }
+  //A : 12345
+  //B : 21232425
+  //C : 3311224455
+}
