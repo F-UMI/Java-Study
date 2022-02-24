1c9ec230a39c9a7e70bc1738d95268ccacfb77ea
윤섭 <zksdbstjq@gmail.com>
2022-02-24 22:03:42 +0900

    Solving : 실패율 스테이지별 실패율 계산 까지 완료

--- Solution/src/FailureRate.java
+++ Solution/src/FailureRate.java
@@ -1,8 +1,42 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N+1];
+
+  private final static int TWO = 2;
+
+  public int[] solution(int N, int[] stages) {
+    int[][] failureRateArray = new int[N + 1][TWO];
+    enterResult(failureRateArray, stages);
+    double[] failureRate = new double[N];
+    calculateRate(failureRateArray, failureRate);
+    for (int[] ints : failureRateArray) {
+      System.out.println(ints[0] + "/" + ints[1]);
+    }
+    return stages;
+  }
+
+  private void enterResult(int[][] failureRateArray, int[] stages) {
+    for (int stage : stages) {
+      if (stage == 0) {
+        failureRateArray[0][0]++;
+      }
+      for (int j = 0; j < stage - 1; j++) {
+        failureRateArray[j + 1][0]++;
+        failureRateArray[j][1]++;
+      }
+    }
+    failureRateArray[0][0] += failureRateArray[0][1];
+  }
+
+  private void calculateRate(int[][] failureRateArray, double[] failureRate) {
+      for (int i = 0; i < failureRate.length; i++) {
+        failureRate[i] = (double) (failureRateArray[i][0] - failureRateArray[i][1]) / failureRateArray[i][0];
+          System.out.println(failureRate[i]);
+      }
+
+  }
+}
+
+
+  /* int[] answer = new int[N+1];
         int[][] stageProgress = new int[N+1][2];
         double[] failureRate = new double[N+1];
         for (int stage : stages){
@@ -33,12 +67,9 @@ class Solution {
             }
         }
 
-        return answer;
-    }
-//
+        return answer;*/
 //    public static void main(String[] args) {
 //        Solution solution = new Solution();
 //        int[] array = {2,1,2,6,2,4,3,3};
 //        solution.solution(5,array);
 //    }
-}
