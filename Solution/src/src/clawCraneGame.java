20c98e54290cf105175270a095469f7809092e2b
윤섭 <zksdbstjq@gmail.com>
2022-03-03 23:45:16 +0900

    Solving : 프로그래머스 크레인 인형 뽑기 문제 오류 수정중

--- Solution/src/src/clawCraneGame.java
+++ Solution/src/src/clawCraneGame.java
@@ -7,27 +7,36 @@ class Solution {
   public int solution(int[][] board, int[] moves) {
     int answer = 0;
     Stack<Integer> basket = new Stack<>();
-    for (int move : moves) {
-      if (moveToBasket(basket, peekPuppet(board, move))) {
-        answer++;
+    basket.push(moves[0]);
+    for (int i = 1; i < moves.length; i++) {
+      System.out.println(moves[i]);
+      if (moveToBasket(basket, peekPuppet(board, moves[i]))) {
+        if (peekPuppet(board, moves[i]) == 0) {
+          continue;
+        }
+        answer += 2;
       }
+
+    }
+    while (!basket.empty()) {
+      System.out.println(basket.pop());
     }
     return answer;
   }
 
   private int peekPuppet(int[][] board, int moves) {
     int puppet = 0;
-    for (int i = 0; i <= board.length; i++) {
-      if (board[i][moves] != 0) {
-        puppet = board[i][moves];
-        board[i][moves] = 0;
+    for (int i = 0; i < board.length; i++) {
+      if (board[i][moves - 1] != 0) {
+        puppet = board[i][moves - 1];
+        board[i][moves - 1] = 0;
+        break;
       }
     }
     return puppet;
   }
 
   private boolean moveToBasket(Stack<Integer> basket, int puppet) {
-    IsEmpty(basket, puppet);
     if (basket.peek() == puppet) {
       basket.pop();
       return true;
@@ -37,10 +46,5 @@ class Solution {
     }
   }
 
-  private void IsEmpty(Stack<Integer> basket, int puppet) {
-    if (basket.empty()) {
-      basket.push(puppet);
-    }
-  }
 
 }
