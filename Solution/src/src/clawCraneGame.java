07a057754c6cb743da3711bcf09f9a8754f04b91
윤섭 <zksdbstjq@gmail.com>
2022-03-04 23:31:11 +0900

    Solving : 프로그래머스 크레인 인형 뽑기 문제 오류 수정중

--- Solution/src/src/clawCraneGame.java
+++ Solution/src/src/clawCraneGame.java
@@ -1,7 +1,7 @@
 package src;
 
 import java.util.Stack;
-
+//뭐가 문제일까
 class Solution {
 
   public int solution(int[][] board, int[] moves) {
@@ -9,7 +9,6 @@ class Solution {
     Stack<Integer> basket = new Stack<>();
     basket.push(moves[0]);
     for (int i = 1; i < moves.length; i++) {
-      System.out.println(moves[i]);
       if (moveToBasket(basket, peekPuppet(board, moves[i]))) {
         if (peekPuppet(board, moves[i]) == 0) {
           continue;
@@ -17,6 +16,12 @@ class Solution {
         answer += 2;
       }
 
+    }
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board.length; j++) {
+        System.out.print(board[i][j]);
+      }
+      System.out.println();
     }
     while (!basket.empty()) {
       System.out.println(basket.pop());
@@ -30,6 +35,7 @@ class Solution {
       if (board[i][moves - 1] != 0) {
         puppet = board[i][moves - 1];
         board[i][moves - 1] = 0;
+
         break;
       }
     }
